15c9135b8132d9354324101685f7b6ec6cb9e596
egoisika <egoisika@yahoo.com>
2013-12-05 15:09:09 +0800

    doctrine + controllers and models

---
+++ module/Application/src/Application/Controller/ProductsController.php
@@ -0,0 +1,64 @@
+<?php
+namespace 
+{
+	class ProductController
+	{
+		public function selectType()
+		{
+			switch ($type)
+			{
+				case 'SunGlasses':
+				# code...
+				break;
+
+				case 'Glasses':
+				# code...
+				break;
+
+				case 'ContactLenses':
+				# code...
+				break;
+				
+				default:
+					# code...
+					break;
+			}
+		}
+
+		private function selectQuery()
+		{
+
+		}
+
+		private function processCSV($file)
+		{
+
+		}
+
+		private function trim()
+		{
+
+		}
+
+		private function exportAsCSV()
+		{
+
+		}
+
+		private function calcSKU()
+		{
+
+		}
+
+		private function addDesc()
+		{
+			
+		}
+
+
+		public function import()
+		{
+
+		}
+	}
+}
---
+++ module/Application/src/Application/Controller/UserController.php
@@ -0,0 +1,21 @@
+<?php
+namespace
+{
+	class UserController 
+	{
+		public function readUser()
+		{
+
+		}
+
+		private function validateUser($firstName, $lastName)
+		{
+
+		}
+
+		private function loggedInUser()
+		{
+			
+		}
+	}
+}
